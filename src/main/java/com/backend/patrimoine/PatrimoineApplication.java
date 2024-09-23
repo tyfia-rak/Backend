721e22554ca1773a3971636d205edbc9f9a38bf7
Sarobidy <sarobidy.rakotonarivo.1@gmail.com>
2024-09-23 14:35:56 +0300

    first commit

---
+++ src/main/java/com/backend/patrimoine/PatrimoineApplication.java
@@ -0,0 +1,13 @@
+package com.backend.patrimoine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimoineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrimoineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/patrimoine/PatrimoineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.patrimoine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimoineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
